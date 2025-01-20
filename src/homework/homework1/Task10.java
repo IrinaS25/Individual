c1e8333e85667aaec8e64b4e9e41735c9bb4f1ef
IrinaS <irundsa@gmail.com>
2025-01-20 12:20:03 +0100

    Add homework 1 Task 10

--- src/homework/homework1/Task10.java
+++ src/homework/homework1/Task10.java
@@ -3,6 +3,9 @@ package homework.homework1;
 import java.util.Scanner;
 
 /*
+Напишите программу, ĸоторая создает массив из 5 строĸ и находит строĸу,
+ĸоторая содержит маĸсимальное ĸоличество пробелов.
+Условие: Используйте циĸл for для поисĸа строĸи, ĸоторая содержит маĸсимальное ĸоличество пробелов.
 
  */
 public class Task10 {
@@ -11,10 +14,37 @@ public class Task10 {
         //создаю массив из 5 строĸ
         String [] array = {"Java ", "is  ", "the   ", "best ", "language    "};
 
+        // создаю переменную для хранения индекса строки с максимальным количеством пробелов
+        int maxSpaceIndex = 0;
 
-        char space = ' ';
+        // создаю переменную для хранения максимального количества числа пробелов
+        int maxSpaceCounter = 0;
 
-        String strMaxSpace = array[0];
+        //цикл for для перебора элементов массива
+        for (int i = 0; i < array.length; i++) {
+
+            // создаю переменную для хранения количества пробелов
+            int spaceCounter = 0;
+
+            //цикл for для перебора элементов строки
+            for (int j = 0; j < array[i].length(); j++) {
+
+                //пишу условие
+                if (array[i].charAt(j) == ' ') {
+                    spaceCounter++;
+                }
+            }
+            //пишу условие
+            if (spaceCounter > maxSpaceIndex) {
+                maxSpaceIndex = spaceCounter;
+                maxSpaceCounter = i;
+            }
+        }
+        System.out.println(array[maxSpaceCounter]);
+
+
+
+
 
 
 
